f9a8fe24df1c5e8ea73e6dc0f868447ed0b1e212
CleuJunior <cleu.junior@gmail.com>
2022-08-09 23:25:18 -0300

    Initial Project

---
+++ src/main/java/com/cleonildo/supermakert/SupermarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.cleonildo.supermakert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleonildo/supermakert/SupermarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleonildo.supermakert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
